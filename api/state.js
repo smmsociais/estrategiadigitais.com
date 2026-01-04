42d8e4572ff115eb25ddf387ffeee0e2827b8a55
SMM <contato@ganhesocial.com>
2026-01-04 18:00:59 -0300

    Update state.js

--- api/state.js
+++ api/state.js
@@ -1,10 +1,16 @@
 import { getDb } from "../lib/mongo.js";
 
 export default async function handler(req, res) {
-  const db = await getDb();
-  const col = db.collection("simulacoes");
+  try {
+    const db = await getDb();
+    const col = db.collection("simulacoes");
 
-  const doc = await col.findOne({ _id: "btc-usdt-sim" });
+    const doc = await col.findOne({ _id: "btc-usdt-sim" });
+    if (!doc) return res.json({ message: "Simulador não iniciado" });
 
-  res.json(doc);
+    res.json(doc);
+  } catch (e) {
+    console.error("state error:", e);
+    res.status(500).json({ error: e.message });
+  }
 }
