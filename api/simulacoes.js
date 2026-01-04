2ca22ea738afb0a40ddca546a88d07ae2840e57b
SMM <contato@ganhesocial.com>
2026-01-04 14:54:39 -0300

    Create simulacoes.js

---
+++ api/simulacoes.js
@@ -0,0 +1,15 @@
+{
+  "_id": "btc-usdt-sim",
+  "saldoBTC": 0.123456,
+  "valorInicial": {
+    "data": "2026-01-04 14:10",
+    "usdt": 10000,
+    "preco": 42000
+  },
+  "valorMaximo": {
+    "data": "2026-01-04 16:32",
+    "usdt": 13240.55,
+    "preco": 55321.12
+  },
+  "updatedAt": "2026-01-04T19:32:00Z"
+}
